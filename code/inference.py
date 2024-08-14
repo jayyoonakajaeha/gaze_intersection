8eac43fac94572d1c8a4a5162a9b820b1464b567
jayyoonakajaeha <148425666+jayyoonakajaeha@users.noreply.github.com>
2024-08-14 23:28:00 +0900

    Update inference.py

--- code/inference.py
+++ code/inference.py
@@ -108,7 +108,7 @@ def test(net, test_image_path, eye):
 
     return heatmap, f_point[0], f_point[1] 
 
-def draw_result(image_path, eye, heatmap, gaze_point,k,i):
+def draw_result(image_path, eye, heatmap, gaze_point,k,i,bounds,paper):
     x1, y1 = eye
     x2, y2 = gaze_point
     im = cv2.imread(image_path)
@@ -120,6 +120,21 @@ def draw_result(image_path, eye, heatmap, gaze_point,k,i):
     cv2.circle(im, (x2, y2), 5, [0, 0, 255], -1)
     cv2.line(im, (x1, y1), (x2, y2), [255, 0, 0], 3)
 
+    for box in bounds:
+        min_x = int(image_width * box['min_x'])
+        max_x = int(image_width * box['max_x'])
+        min_y = int(image_height * box['min_y'])
+        max_y = int(image_height * box['max_y'])
+        # Draw rectangle on image
+        cv2.rectangle(im, (min_x, min_y), (max_x, max_y), [0, 255, 0], 2)
+
+    min_x = int(image_width * paper['min_x'])
+    max_x = int(image_width * paper['max_x'])
+    min_y = int(image_height * paper['min_y'])
+    max_y = int(image_height * paper['max_y'])
+    # Draw rectangle on image
+    cv2.rectangle(im, (min_x, min_y), (max_x, max_y), [0, 255, 255], 2)
+
     # heatmap visualization
     heatmap = ((heatmap - heatmap.min()) / (heatmap.max() - heatmap.min()) * 255).astype(np.uint8)
     heatmap = np.stack([heatmap, heatmap, heatmap], axis=2)
@@ -127,10 +142,24 @@ def draw_result(image_path, eye, heatmap, gaze_point,k,i):
 
     heatmap = (0.8 * heatmap.astype(np.float32) + 0.2 * im.astype(np.float32)).astype(np.uint8)
     img = np.concatenate((im, heatmap), axis=1)
-    cv2.imwrite(f'./results/tmp_{k}_{i}.png', img)
+    cv2.imwrite(f'D:/group2_results/tmp_{k}_{i}.png', img)
     
     return img
     
+
+def paper_coor(frame_width, frame_height):
+    min_x = 540 / frame_width
+    max_x = 1470 /frame_width
+    min_y = 755 / frame_height
+    max_y = 1060 /frame_height
+    paper={
+        'min_y': min_y,
+        'max_y': max_y,
+        'min_x': min_x,
+        'max_x': max_x
+    }
+    return paper
+
 def main():
 
     net = GazeNet()
@@ -138,25 +167,41 @@ def main():
     net.cuda()
 
     pretrained_dict = torch.load('../model/pretrained_model.pkl')
+    frame_num = 61894 #number of frame
     model_dict = net.state_dict()
     pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
     model_dict.update(pretrained_dict)
     net.load_state_dict(model_dict)
-    f= open("./log/log.txt","w+")
+    f= open("./log/group2_1_log.txt","w+")
+    
     cnt=0
-    for k in range(150):
-        test_image_path = f'../../imgs/frame_{k:05d}.png'
-        input_path = f'../../../openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended/openpose/output/3_{k:012d}_keypoints.json'
+    personal_cnt=[0,0,0,0,0,0,0,0,0]
+    flag=[0,0,0,0,0,0,0,0,0]
+    personal_rcnt=[0,0,0,0,0,0,0,0,0]
+
+    paper_cnt=0
+    paper_personal_cnt=[0,0,0,0,0,0,0,0,0]
+    paper_flag=[0,0,0,0,0,0,0,0,0]
+    paper_personal_rcnt=[0,0,0,0,0,0,0,0,0]
+
+    paper = paper_coor(1920,1080)
+    for k in range(frame_num):
+        test_image_path = f'D:/group2_imgs/frame_{k:05d}.png'
+        input_path = f'../../../openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended/openpose/output/group2_{k:012d}_keypoints.json'
         print(input_path)
         coor = normalize_coordinates(input_path,1920,1080)
         f.write(f'<frame {k}>\n')
+        is_gazing_1 = 0
+        paper_gazing1 = 0
         for i,people in enumerate(coor):
+            is_gazing=0
+            paper_gazing=0
             x = people['norm_eye_x']
             y = people['norm_eye_y']
             heatmap, p_x, p_y = test(net, test_image_path, (x, y))
-            draw_result(test_image_path, (x, y), heatmap, (p_x, p_y),k,i)
             f.write(f'person {i}\n{p_x} {p_y}\n')
             bounds = bound(input_path,1920,1080)
+            draw_result(test_image_path, (x, y), heatmap, (p_x, p_y),k,i,bounds,paper)
             for j,peo in enumerate(bounds):
                 if i!=j:
                     min_x = peo['min_x']
@@ -165,12 +210,57 @@ def main():
                     max_y = peo['max_y']
                     #print(min_x,max_x,min_y,max_y)
                     if p_x>=min_x and p_x<=max_x and p_y>=min_y and p_y<=max_y:
-                        cnt+=1
+                        is_gazing=1
+                        is_gazing_1=1
                     f.write(f"person {j}'s bounding box\n{min_x} {max_x} {min_y} {max_y}\n")
+                    if p_x>=paper['min_x'] and p_x<=paper['max_x'] and p_y>=paper['min_y'] and p_y<=paper['max_y']:
+                        paper_gazing1=1
+                        paper_gazing=1
+                        
+            personal_cnt[i]+=is_gazing
+            paper_personal_cnt[i]+=paper_gazing
+            if is_gazing==1:
+                if flag[i]==0:
+                    personal_rcnt[i]+=1
+                    flag[i]=1
+            else:
+                flag[i]=0
+
+            if paper_gazing==1:
+                if paper_flag[i]==0:
+                    paper_personal_rcnt[i]+=1
+                    paper_flag[i]=1
+            else:
+                paper_flag[i]=0
             f.write('\n')
-        f.write(f'cnt: {cnt}\n\n')
-        #print(cnt)
+
+        cnt+=is_gazing_1
+        paper_cnt+=paper_gazing1
+
+        f.write(f"personal cnt: ")
+        for i in range(len(personal_cnt)):
+            f.write(f"{i}:{personal_cnt[i]} ")
+        print(personal_cnt)
+        f.write(f"\npersonal real cnt: ")
+        for i in range(len(personal_rcnt)):
+            f.write(f"{i}:{personal_rcnt[i]} ")
+        print(personal_rcnt)
+        f.write(f'\ncnt: {cnt}\n\n')
+
+        f.write(f"paper gaze personal cnt: ")
+        for i in range(len(paper_personal_cnt)):
+            f.write(f"{i}:{paper_personal_cnt[i]} ")
+        print(paper_personal_cnt)
+        f.write(f"\npaper gaze personal real cnt: ")
+        for i in range(len(paper_personal_rcnt)):
+            f.write(f"{i}:{paper_personal_rcnt[i]} ")
+        print(paper_personal_rcnt)
+        f.write(f'\npaper gaze cnt: {paper_cnt}\n\n')
+
+        print(cnt)
+        print(paper_cnt)
     f.close()
+    print(cnt)
 
     
 if __name__ == '__main__':
