0b7c721b3fa4ab4e1fd531ad4c0a7703729d3bbd
jayyoonakajaeha <148425666+jayyoonakajaeha@users.noreply.github.com>
2024-08-14 23:26:30 +0900

    Update eye_position_norm.py

--- code/eye_position_norm.py
+++ code/eye_position_norm.py
@@ -10,8 +10,6 @@ def normalize_coordinates(input_file, frame_width, frame_height):
 
     # 눈 좌표 가져오기 및 정규화
     for person in data['people']:
-        # 좌표 가져오기
-        # 1번과 2번 keypoints가 각각 왼쪽 눈과 오른쪽 눈의 x, y 좌표임
         keypoints = person['pose_keypoints_2d']
         left_eye_x, left_eye_y = keypoints[3*16:3*16+2]
         right_eye_x, right_eye_y = keypoints[3*15:3*15+2]
@@ -22,20 +20,27 @@ def normalize_coordinates(input_file, frame_width, frame_height):
         normalized_right_eye_x = right_eye_x / frame_width
         normalized_right_eye_y = right_eye_y / frame_height
 
-        norm_eye_x = normalized_left_eye_x
-        norm_eye_y = normalized_left_eye_y
-        if norm_eye_x==0.0:
-            norm_eye_x = normalized_right_eye_x
-            norm_eye_y = normalized_right_eye_y
+        #if normalized_left_eye_x!=0 and normalized_right_eye_x!=0:
+        #    norm_eye_x = (normalized_left_eye_x + normalized_right_eye_x)/2.0
+        #    norm_eye_y = (normalized_left_eye_y+normalized_right_eye_y)/2.0
+        if True:
+            norm_eye_x = normalized_left_eye_x
+            norm_eye_y = normalized_left_eye_y
+            if norm_eye_y<normalized_right_eye_y:
+                norm_eye_x = normalized_right_eye_x
+                norm_eye_y = normalized_right_eye_y
     
 
         # 새로운 키 추가
-        norm_eyes.append({
-            'norm_eye_x':norm_eye_x,
-            'norm_eye_y':norm_eye_y
-        })
+        if norm_eye_x!=0 and norm_eye_y!=0:
+            norm_eyes.append({
+                'norm_eye_x':norm_eye_x,
+                'norm_eye_y':norm_eye_y
+            })
+    # norm_eyes 리스트를 제자리에서 정렬
+    norm_eyes.sort(key=lambda eye: eye['norm_eye_x'])
+  
 
-    # 결과를 새로운 JSON 파일로 저장
     return norm_eyes
 
 # 사용 예시
